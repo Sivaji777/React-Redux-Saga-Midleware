933ed13cd61cc0f13e5d15e3b54eab1e3ae0249e
Sivaji777 <psivaji777@gmail.com>
2022-04-07 13:01:02 +0530

    React Redux-Saga Middleware

---
+++ App.js
@@ -0,0 +1,11 @@
+import MobileDetailes from "./Redux-Saga/MobileDetailes";
+
+function App() {
+  return (
+    <div className="App">
+     <MobileDetailes />
+    </div>
+  );
+}
+
+export default App;
